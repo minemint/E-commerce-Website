2de4ca46dcfdfe05532cd654e2c48027f4b49ba3
nateethorn <106004714+minemint@users.noreply.github.com>
2022-07-21 20:29:58 +0700

    python
    ทวนถึงการตรวจสอบข้อมูล

---
+++ testcs.cs
@@ -0,0 +1,16 @@
+//โชว์ข้อความhello
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace testcs
+//{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
